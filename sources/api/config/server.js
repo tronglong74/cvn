f4ce099f2602ddee324300d0e24144771f2de6d8
Phuong Long <long@motbit.com>
2021-11-16 23:19:35 +0700

    update config

--- sources/api/config/server.js
+++ sources/api/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
+    url: '/sitemanager',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '1b8b63bbcb6061ef52ff2b0abf7c8f24'),
     },
